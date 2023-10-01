debc4bc00683dd61fc6c3a7a17c648e24d39d839
Sofia Ruban <sophiaruban249324@gmail.com>
2023-10-01 20:07:34 +0300

    feat: Complete signup functionality

--- real-estate-app/api/controllers/auth.controller.js
+++ real-estate-app/api/controllers/auth.controller.js
@@ -14,7 +14,7 @@ export const signup = async (req, res, next) => {
     await newUser.save()
     res.status(201).json("User created successfully")
   } catch (error) {
-    next(errorHandler(550, "error from the function"))
+    next(errorHandler(error))
   }
   
 }
