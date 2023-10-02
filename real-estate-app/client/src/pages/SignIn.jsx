7e0818f7d245ad6d95b7f9ed3e0e3cfde4464eba
Sofia Ruban <sophiaruban249324@gmail.com>
2023-10-02 22:09:43 +0300

    feat: Add OAuth functionality

--- real-estate-app/client/src/pages/SignIn.jsx
+++ real-estate-app/client/src/pages/SignIn.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from "react-redux"
 import { signInStart, signInSuccess, signInFailure} from '../redux/user/userSlice'
+import Oauth from '../components/Oauth'
 
 export default function SignIn() {
   const [formData, setFormData] = useState({})
@@ -62,6 +63,7 @@ export default function SignIn() {
         >
           {loading ? 'Loading...' : 'Sign in'}
         </button>
+        <Oauth />
       </form>
       <div className="flex gap-2 mt-5">
         <p>Dont have an account?</p>
